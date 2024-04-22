f77bfe2e306d0457fa009aad912e91a6c7a22b8a
zhuoyu tan <georgetanyeah@gmail.com>
2024-04-22 16:42:43 -0700

    fix mongoDB connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(express.static('public'))
   
   const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   const db = mongoose.connection
   db.on('error', error => console.error(error))
   db.once('open', () => console.log('Connected to Mongoose'))
